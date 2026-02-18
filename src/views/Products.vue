798e7a3688f3e8a4e3fabd2fb130fe35c2e84432
Hetziel <90333383+hetziel@users.noreply.github.com>
2026-02-18 10:54:15 -0400

    Mejoras

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -265,7 +265,7 @@
                 <div class="price-input">
                   <span class="price-prefix">{{
                     handleProduct.currency_type === 'USD' ? '$' : 'Bs'
-                    }}</span>
+                  }}</span>
                   <input v-model.number="handleProduct.tempPrice" type="number" min="0" step="0.01" class="form-input"
                     placeholder="0.00" />
                 </div>
@@ -453,7 +453,7 @@
             <div class="est-name font-bold">Promedio</div>
             <div class="average-prices-inline text-right">
               <span class="font-bold text-primary">${{ (selectedProductForPrices.average_price || 0).toFixed(2)
-                }}</span>
+              }}</span>
               <span class="text-muted mx-2">|</span>
               <span class="text-muted">Bs {{ ((selectedProductForPrices.average_price || 0) * (dolarBCV?.promedio ||
                 0)).toFixed(2) }}</span>
@@ -632,11 +632,15 @@
                 <div v-else class="product-badge" style="background-color: #6b7280">
                   <span>{{
                     getMeasurementType(product.measurement_id)?.charAt(0) || 'P'
-                    }}</span>
+                  }}</span>
                 </div>
                 <div class="product-details">
                   <h3 class="product-name">
                     {{ product.name }}
+
+                    <template v-if="product.brand_id">
+                      - {{ getBrandName(product.brand_id) }}
+                    </template>
                     <Icon v-if="product.type === 'alimento'" name="grain" class="type-icon food" title="Alimento" />
                     <Icon v-if="product.type === 'pollo'" name="bird" class="type-icon chicken"
                       title="Pollo de Engorde" />
